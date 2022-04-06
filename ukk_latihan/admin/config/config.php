1b626b72d78c0f201eb2bc511464ba24ca50506c
Desitrisundari <97926003+Desitrisundari@users.noreply.github.com>
2022-04-06 16:04:34 +0700

    Add files via upload

---
+++ ukk_latihan/admin/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('baseurl','http://localhost/ukk_latihan/public');
+
+//db
+define('DB_HOST','localhost');
+define('DB_USER','root');
+define('DB_PASS','');
+define('DB_NAME','db_portofolio');
---
+++ ukk_latihan/admin/init.php
@@ -0,0 +1,7 @@
+<?php 
+//menghubungkan ke App.php yang mengatur URL
+require_once 'core/App.php';
+//menghubungkan ke contrloller utama
+require_once 'core/Controller.php';
+//menghubungkan ke file kongigurasi
+require_once 'config/config.php';
